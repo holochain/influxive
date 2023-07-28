f8ae6e389e9493601eb6e3c62cbb57183df88f6d
neonphog <neonphog@gmail.com>
2023-07-28 12:15:40 -0600

    better write buffer

--- crates/influxive-writer/src/lib.rs
+++ crates/influxive-writer/src/lib.rs
@@ -30,6 +30,7 @@
 //! ```
 
 use influxive_core::*;
+use std::sync::Arc;
 
 trait DataTypeExt {
     fn into_type(self) -> influxdb::Type;
@@ -47,19 +48,133 @@ impl DataTypeExt for DataType {
     }
 }
 
+/// Backend types you probably don't need.
+pub mod types {
+    use super::*;
+
+    /// backend
+    pub trait Backend: 'static + Send + Sync {
+        /// buffer a metric
+        fn buffer_metric(&mut self, metric: Metric);
+
+        /// get count of buffered metrics
+        fn buffer_count(&self) -> usize;
+
+        /// send buffered metrics
+        fn send(
+            &mut self,
+        ) -> std::pin::Pin<
+            Box<dyn std::future::Future<Output = ()> + '_ + Send + Sync>,
+        >;
+    }
+
+    /// factory
+    pub trait BackendFactory: std::fmt::Debug + 'static + Send + Sync {
+        /// create a new influxdb backend connector via token auth
+        fn with_token_auth(
+            &self,
+            host: String,
+            bucket: String,
+            token: String,
+        ) -> Box<dyn Backend + 'static + Send + Sync>;
+    }
+
+    struct DefaultBackend {
+        buffer: Vec<influxdb::WriteQuery>,
+        client: influxdb::Client,
+    }
+
+    impl Backend for DefaultBackend {
+        fn buffer_metric(&mut self, metric: Metric) {
+            let Metric {
+                timestamp,
+                name,
+                fields,
+                tags,
+            } = metric;
+
+            let mut query = influxdb::WriteQuery::new(
+                influxdb::Timestamp::Nanoseconds(
+                    timestamp
+                        .duration_since(std::time::SystemTime::UNIX_EPOCH)
+                        .unwrap()
+                        .as_nanos(),
+                ),
+                name.into_string(),
+            );
+
+            for (k, v) in fields {
+                query = query.add_field(k.into_string(), v.into_type());
+            }
+
+            for (k, v) in tags {
+                query = query.add_tag(k.into_string(), v.into_type());
+            }
+
+            self.buffer.push(query);
+        }
+
+        fn buffer_count(&self) -> usize {
+            self.buffer.len()
+        }
+
+        fn send(
+            &mut self,
+        ) -> std::pin::Pin<
+            Box<dyn std::future::Future<Output = ()> + '_ + Send + Sync>,
+        > {
+            Box::pin(async move {
+                if let Err(err) =
+                    self.client.query(std::mem::take(&mut self.buffer)).await
+                {
+                    tracing::warn!(?err, "write metrics error");
+                }
+            })
+        }
+    }
+
+    /// currently backed by the crate influxdb,
+    /// but subject to change without notice
+    #[derive(Debug)]
+    pub struct DefaultBackendFactory;
+
+    impl BackendFactory for DefaultBackendFactory {
+        fn with_token_auth(
+            &self,
+            host: String,
+            bucket: String,
+            token: String,
+        ) -> Box<dyn Backend + 'static + Send + Sync> {
+            let client = influxdb::Client::new(host, bucket).with_token(token);
+            let out: Box<dyn Backend + 'static + Send + Sync> =
+                Box::new(DefaultBackend {
+                    buffer: Vec::new(),
+                    client,
+                });
+            out
+        }
+    }
+}
+
 /// InfluxDB metric writer configuration.
 #[derive(Debug, Clone)]
 pub struct InfluxiveWriterConfig {
-    /// Time span over which metric writes will be buffered before
+    /// Max time span over which metric writes will be buffered before
     /// actually being written to InfluxDB to facilitate batching.
     /// Defaults to `100ms`.
     pub batch_duration: std::time::Duration,
 
     /// The size of the metric write batch buffer. If a metric to be
-    /// writen would go beyond this buffer, it will instead be ignored with
-    /// a trace written at "debug" level.
+    /// writen would go beyond this buffer, the batch will be sent early.
+    /// If the buffer is again full before the previous batch finishes
+    /// sending, the metric will be ignored and a trace will be written
+    /// at "debug" level.
     /// Defaults to `4096`.
     pub batch_buffer_size: usize,
+
+    /// Backend driving this writer instance. This is currently driven
+    /// by the influxdb crate, but that is subject to change without notice.
+    pub backend: Arc<dyn types::BackendFactory + 'static + Send + Sync>,
 }
 
 impl Default for InfluxiveWriterConfig {
@@ -67,12 +182,62 @@ impl Default for InfluxiveWriterConfig {
         Self {
             batch_duration: std::time::Duration::from_millis(100),
             batch_buffer_size: 4096,
+            backend: Arc::new(types::DefaultBackendFactory),
         }
     }
 }
 
+enum WriteCmd {
+    Timeout,
+    Metric(Metric),
+}
+
+struct WriteBuf {
+    config: InfluxiveWriterConfig,
+    backend: Box<dyn types::Backend + 'static + Send + Sync>,
+    last_send: std::time::Instant,
+}
+
+type ShouldSend = bool;
+
+impl WriteBuf {
+    pub fn new(
+        config: InfluxiveWriterConfig,
+        backend: Box<dyn types::Backend + 'static + Send + Sync>,
+    ) -> Self {
+        Self {
+            config,
+            backend,
+            last_send: std::time::Instant::now(),
+        }
+    }
+
+    pub fn process(&mut self, cmd: WriteCmd) -> ShouldSend {
+        match cmd {
+            WriteCmd::Timeout => {
+                self.backend.buffer_count() > 0
+                    && self.last_send.elapsed() >= self.config.batch_duration
+            }
+            WriteCmd::Metric(metric) => {
+                if self.backend.buffer_count() == 0 {
+                    self.last_send = std::time::Instant::now();
+                }
+
+                self.backend.buffer_metric(metric);
+
+                self.backend.buffer_count() >= self.config.batch_buffer_size
+                    || self.last_send.elapsed() >= self.config.batch_duration
+            }
+        }
+    }
+
+    pub async fn send(&mut self) {
+        self.backend.send().await;
+    }
+}
+
 /// InfluxDB metric writer instance.
-pub struct InfluxiveWriter(tokio::sync::mpsc::Sender<Metric>);
+pub struct InfluxiveWriter(tokio::sync::mpsc::Sender<WriteCmd>);
 
 impl InfluxiveWriter {
     /// Construct a new writer authenticated by a token.
@@ -82,51 +247,44 @@ impl InfluxiveWriter {
         bucket: B,
         token: T,
     ) -> Self {
-        let client = influxdb::Client::new(host.as_ref(), bucket.as_ref())
-            .with_token(token.as_ref());
+        let backend = config.backend.with_token_auth(
+            host.as_ref().to_string(),
+            bucket.as_ref().to_string(),
+            token.as_ref().to_string(),
+        );
 
         let (write_send, mut write_recv) =
             tokio::sync::mpsc::channel(config.batch_buffer_size);
 
-        let write_sleep_dur = config.batch_duration;
+        let write_send_timer = write_send.clone();
+        let mut interval = tokio::time::interval(config.batch_duration / 3);
         tokio::task::spawn(async move {
-            let mut q = Vec::new();
-
             loop {
-                tokio::time::sleep(write_sleep_dur).await;
+                interval.tick().await;
+                if write_send_timer.send(WriteCmd::Timeout).await.is_err() {
+                    break;
+                }
+            }
+        });
+
+        let mut write_buf = WriteBuf::new(config, backend);
+
+        tokio::task::spawn(async move {
+            while let Some(cmd) = write_recv.recv().await {
+                if write_buf.process(cmd) {
+                    write_buf.send().await;
+                }
 
                 loop {
                     match write_recv.try_recv() {
-                        Ok(Metric {
-                            timestamp,
-                            name,
-                            fields,
-                            tags,
-                        }) => {
-                            let mut query = influxdb::WriteQuery::new(
-                                influxdb::Timestamp::Nanoseconds(
-                                    timestamp
-                                        .duration_since(std::time::SystemTime::UNIX_EPOCH)
-                                        .unwrap()
-                                        .as_nanos()
-                                ),
-                                name.into_string(),
-                            );
-                            for (k, v) in fields {
-                                query = query.add_field(k.into_string(), v.into_type());
+                        Ok(cmd) => {
+                            if write_buf.process(cmd) {
+                                write_buf.send().await;
                             }
-                            for (k, v) in tags {
-                                query = query.add_tag(k.into_string(), v.into_type());
-                            }
-                            q.push(query)
                         }
                         Err(tokio::sync::mpsc::error::TryRecvError::Empty) => break,
                         Err(tokio::sync::mpsc::error::TryRecvError::Disconnected) => return,
                     }
-                }
-
-                if let Err(err) = client.query(std::mem::take(&mut q)).await {
-                    tracing::warn!(?err, "write metrics error");
                 }
             }
         });
@@ -140,7 +298,7 @@ impl InfluxiveWriter {
     /// The actual call to log the metrics will be made a configurable
     /// timespan later to facilitate batching of metric writes.
     pub fn write_metric(&self, metric: Metric) {
-        match self.0.try_send(metric) {
+        match self.0.try_send(WriteCmd::Metric(metric)) {
             Ok(()) => (),
             Err(tokio::sync::mpsc::error::TrySendError::Full(_)) => {
                 tracing::warn!("metrics overloaded, dropping metric");
@@ -157,3 +315,6 @@ impl influxive_core::MetricWriter for InfluxiveWriter {
         InfluxiveWriter::write_metric(self, metric);
     }
 }
+
+#[cfg(test)]
+mod test;
---
+++ crates/influxive-writer/src/test.rs
@@ -0,0 +1,99 @@
+use crate::types::*;
+use crate::*;
+
+struct TestBackend(u64, Arc<std::sync::atomic::AtomicU64>);
+
+impl Backend for TestBackend {
+    fn buffer_metric(&mut self, _metric: Metric) {
+        self.0 += 1;
+    }
+
+    fn buffer_count(&self) -> usize {
+        self.0 as usize
+    }
+
+    fn send(
+        &mut self,
+    ) -> std::pin::Pin<
+        Box<dyn std::future::Future<Output = ()> + '_ + Send + Sync>,
+    > {
+        Box::pin(async move {
+            // simulate it taking a while to do things
+            tokio::time::sleep(std::time::Duration::from_millis(5)).await;
+            self.1
+                .fetch_add(self.0, std::sync::atomic::Ordering::SeqCst);
+            self.0 = 0;
+        })
+    }
+}
+
+#[derive(Debug)]
+struct TestFactory {
+    write_count: Arc<std::sync::atomic::AtomicU64>,
+}
+
+impl TestFactory {
+    pub fn new() -> Arc<Self> {
+        Arc::new(Self {
+            write_count: Arc::new(std::sync::atomic::AtomicU64::new(0)),
+        })
+    }
+
+    pub fn get_write_count(&self) -> u64 {
+        self.write_count.load(std::sync::atomic::Ordering::SeqCst)
+    }
+}
+
+impl BackendFactory for TestFactory {
+    fn with_token_auth(
+        &self,
+        _host: String,
+        _bucket: String,
+        _token: String,
+    ) -> Box<dyn Backend + 'static + Send + Sync> {
+        let out: Box<dyn Backend + 'static + Send + Sync> =
+            Box::new(TestBackend(0, self.write_count.clone()));
+        out
+    }
+}
+
+#[tokio::test(flavor = "multi_thread")]
+async fn writer_stress() {
+    let factory = TestFactory::new();
+
+    let config = InfluxiveWriterConfig {
+        batch_duration: std::time::Duration::from_millis(3),
+        batch_buffer_size: 10,
+        backend: factory.clone(),
+    };
+
+    let writer = InfluxiveWriter::with_token_auth(config, "", "", "");
+
+    // this should be well within our cadence
+    for _ in 0..5 {
+        for _ in 0..5 {
+            writer.write_metric(
+                Metric::new(std::time::SystemTime::now(), "my.metric")
+                    .with_field("val", 3.14)
+                    .with_tag("tag", "test-tag"),
+            );
+        }
+        tokio::time::sleep(std::time::Duration::from_millis(20)).await;
+    }
+
+    assert_eq!(25, factory.get_write_count());
+
+    // this should be well outside our cadence
+    for _ in 0..5 {
+        for _ in 0..500 {
+            writer.write_metric(
+                Metric::new(std::time::SystemTime::now(), "my.metric")
+                    .with_field("val", 3.14)
+                    .with_tag("tag", "test-tag"),
+            );
+        }
+        tokio::time::sleep(std::time::Duration::from_millis(20)).await;
+    }
+
+    assert!(factory.get_write_count() < 2500);
+}
